91ca624f917d0ffc6b115ce52b4be444eef94c7b
Nguyễn Chí Cường <cuongnguyengl2003@gmail.com>
2024-05-25 12:13:56 +0700

    test

--- Blog/index.js
+++ Blog/index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// test route
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+// 
 app.listen(port, () => {
   console.log(`Example app listening on port https:/localhost/${port}`);
 });
